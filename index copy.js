23b3eb911f73e0183b1ce76e0a13e12c138d540b
stef-geo <s7efbg@gmail.com>
2024-09-16 23:42:15 -0300

    atualizei

---
+++ index copy.js
@@ -0,0 +1,7 @@
+var marcas = 'Adidas'
+console.log(`${marcas}`)
+if (marcas == 'Adidas') {
+    console.log('Você gosta de Adidas.')
+} else if (marcas == 'Nike') {
+    console.log('voce gosta nike')
+}
