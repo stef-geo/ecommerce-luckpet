04745b0b4ba43675867fa6afd67e5e65cad22649
stef-geo <s7efbg@gmail.com>
2024-11-18 16:55:44 -0300

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Configurar o EJS como motor de visualização
 app.set('view engine', 'ejs');
