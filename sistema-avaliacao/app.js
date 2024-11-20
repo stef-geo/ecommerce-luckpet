6e88e110068771bb25cc30ff6b2b67e91b4bf856
stef-geo <s7efbg@gmail.com>
2024-11-20 11:13:41 -0300

    update

--- sistema-avaliacao/app.js
+++ sistema-avaliacao/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 4000;
+const port = 4040;
 
 // Middleware
 app.use(cors());
@@ -60,7 +60,7 @@ app.get('/avaliacoes', (req, res) => {
 });
 
 // Inicialização do servidor
-app.listen(4000, '0.0.0.0');
+app.listen(4040, '0.0.0.0');
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
